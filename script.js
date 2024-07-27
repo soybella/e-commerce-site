578c90b261f0191d42e8a34057f7fd8a328bb445
Isabella <isabellaj.lizarde@gmail.com>
2024-07-27 00:05:21 -0700

    Created function to open/close responsive nav bar

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ function handleShoppingCartButton() {
 }
 
 function handleNavBarIcon() {
-  let openNavBar = document.querySelector(".navbar-responsive");
-  openNavBar.classList.toggle("hidden");
+  let responsiveNavBar = document.querySelector(".navbar-responsive");
+  responsiveNavBar.style.display = "block";
 }
+
+function handleCloseNavBar() {
+  let responsiveNavBar = document.querySelector(".navbar-responsive");
+  responsiveNavBar.style.display = "none";
+}
+
+let closeNavBar = document.querySelector("#navbar-close-button");
+closeNavBar.addEventListener("click", handleCloseNavBar);
 
 let navBarIcon = document.querySelector(".nav-menu-icon");
 navBarIcon.addEventListener("click", handleNavBarIcon);
