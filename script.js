5c1bfd5f42c7557c6f4551aff0c284b3566f1b3f
Isabella <isabellaj.lizarde@gmail.com>
2024-07-30 14:41:27 -0700

    Updated thumbnails button functionality
    When small thumbnails are clicked it will update the larger main product image

--- script.js
+++ script.js
@@ -13,6 +13,22 @@ function handleCloseNavBar() {
   responsiveNavBar.style.display = "none";
 }
 
+function handleProductThumbnailSmall(event) {
+  console.log("Thumbnail clicked");
+  let clickedThumbnail = event.target;
+  let newMainThumbnail = clickedThumbnail.getAttribute("data-large-src");
+  let productThumbnailMain = document.querySelector(".product-thumbnail-main");
+
+  if (productThumbnailMain && newMainThumbnail) {
+    productThumbnailMain.src = newMainThumbnail;
+  }
+}
+
+let productThumbnailSmall = document.querySelectorAll(".product-small-wrapper");
+productThumbnailSmall.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", handleProductThumbnailSmall);
+});
+
 let closeNavBar = document.querySelector("#navbar-close-button");
 closeNavBar.addEventListener("click", handleCloseNavBar);
 
