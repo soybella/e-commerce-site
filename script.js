3465aa182d0051a47444baf4dfbfb84574c5ef1d
Isabella <isabellaj.lizarde@gmail.com>
2024-08-06 00:13:55 -0700

    Updated button functionality for photos
    Clicking through small thumbnails will change the main photo on different screen sizes and when the light box is visible

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const productThumbnailSmall = document.querySelectorAll(
   "main .product-small-wrapper"
 );
+let productThumbnailSmallLightBox = document.querySelectorAll(
+  "section .product-small-wrapper"
+);
 const previousButton = document.querySelector(".previous-button");
 const nextButton = document.querySelector(".next-button");
 const closeNavBar = document.querySelector("#navbar-close-button");
@@ -9,11 +12,19 @@ const navBarIcon = document.querySelector(".nav-menu-icon");
 let displayLightBox = document.querySelector(".light-box");
 let closeLightBox = document.querySelector("#light-box-close-button");
 let productThumbnailMain = document.querySelector(".product-thumbnail-main");
+let productThumbnailMainLightbox = document.querySelector(
+  ".light-box .product-thumbnail-main"
+);
 let currentPhotoIndex = 0;
 
 closeNavBar.addEventListener("click", handleCloseNavBar);
 navBarIcon.addEventListener("click", handleNavBarIcon);
 shoppingCartButton.addEventListener("click", handleShoppingCartButton);
+// Figure out why this is not working
+// productThumbnailSmallLightBox.addEventListener(
+//   "click",
+//   handleProductThumbnailSmall
+// );
 
 closeLightBox.addEventListener("click", () => {
   displayLightBox.classList.add("hidden");
@@ -45,6 +56,7 @@ previousButton.addEventListener("click", () => {
 nextButton.addEventListener("click", () => {
   if (currentPhotoIndex < productThumbnailSmall.length - 1) {
     currentPhotoIndex++;
+    // nextButton.disabled = true;
     updatePhoto();
   }
 });
@@ -56,8 +68,19 @@ productThumbnailSmall.forEach((thumbnail, index) => {
   });
 });
 
+productThumbnailSmallLightBox.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", () => {
+    currentPhotoIndex = index;
+    updatePhoto();
+  });
+});
+
 productThumbnailSmall.forEach((thumbnail) => {
   thumbnail.addEventListener("click", handleProductThumbnailSmall);
+});
+
+productThumbnailSmallLightBox.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", handleProductThumbnailSmallLightBox);
 });
 
 function handleShoppingCartButton() {
@@ -74,27 +97,55 @@ function handleCloseNavBar() {
   let responsiveNavBar = document.querySelector(".navbar-responsive");
   responsiveNavBar.style.display = "none";
 }
+// if product thumbnail small has class of .light-box choose that variable instead
 
 function handleProductThumbnailSmall(event) {
   let clickedThumbnail = event.target;
   let newMainThumbnail = clickedThumbnail.getAttribute("data-large-src");
+  let newMainThumbnailLightBox =
+    clickedThumbnail.getAttribute("data-large-src");
+  //   console.log(newMainThumbnailLightBox);
   //   let productThumbnailMain = document.querySelector(".product-thumbnail-main");
 
   if (productThumbnailMain && newMainThumbnail) {
     productThumbnailMain.src = newMainThumbnail;
+    productThumbnailMainLightbox.src = newMainThumbnailLightBox;
+  }
+}
+
+function handleProductThumbnailSmallLightBox(event) {
+  let clickedThumbnail = event.target;
+
+  let newMainThumbnailLightBox =
+    clickedThumbnail.getAttribute("data-large-src");
+
+  if (productThumbnailMainLightbox && newMainThumbnailLightBox) {
+    productThumbnailMainLightbox.src = newMainThumbnailLightBox;
   }
 }
 
 function updatePhoto() {
   let newPhotoSrc =
     productThumbnailSmall[currentPhotoIndex].getAttribute("data-large-src");
+
+  let newPhotoSrcLightBox =
+    productThumbnailSmallLightBox[currentPhotoIndex].getAttribute(
+      "data-large-src"
+    );
+
   let photos = document.querySelectorAll(".photo");
+  let photosLightBox = document.querySelectorAll(".photo-light-box");
+
   let newMainPhoto = Array.from(photos).find(
     (photo) => photo.getAttribute("data-large-src") === newPhotoSrc
   );
 
+  let newMainPhotoLightBox = Array.from(photosLightBox).find(
+    (photo) => photo.getAttribute("data-large-src") === newPhotoSrcLightBox
+  );
+
   if (newMainPhoto) {
-    productThumbnailMain.src = newPhotoSrc;
+    productThumbnailMainLightbox.src = newMainPhotoLightBox;
   } else {
     return newPhotoSrc;
   }
@@ -105,7 +156,7 @@ function updatePhoto() {
     previousButton.disabled = false;
   }
 
-  if (currentPhotoIndex === productThumbnailSmall.length - 1) {
+  if (currentPhotoIndex === 4) {
     nextButton.disabled = true;
   } else {
     nextButton.disabled = false;
